031ceb9cf20d5635c5311f0bd6a1e8f81d5f3f63
Adii-tii <sableaditi8@gmail.com>
2026-01-23 15:18:33 +0530

    implemented login/register apis

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const { prototype } = require('events');
 const express = require('express'); //legacy way of importing
 const fs = require('fs');
 
 const app = express(); // configuring express app
+const PORT = 5001;
 
 app.use(express.json()); //middleware to parse json body. It intercepts all incoming requests before the acutal code executes.
 /*middleware is a function that runs between request and response cycle. used to 
@@ -11,6 +13,8 @@ app.use(express.json()); //middleware to parse json body. It intercepts all inco
 4. handling cors errors 
 5. security headers*/
 
+let users = [];
+let id = 1;
 app.post('/register', (req, res) => {
     const {name, email, password} = req.body;
 
@@ -19,16 +23,52 @@ app.post('/register', (req, res) => {
             messages: 'Name, email and password are required!'
         })
     }
+    else if(users.some(user => user.email === email)){ //Determines whether the specified callback function returns true for any element of an array.
 
-    fs.appendFileSync("users.txt", `${name} ${email} ${password}\n`);
-    res.status(200);
-    res.send("registered successfully");
-    console.log(`${name} has registered.`);
+        return res.status(400).json({
+            message: 'User with this email already exists.'
+        })
+    } 
+
+    const user = {
+        name : name,
+        email : email,
+        password : password
+    }
+    
+    users.push(user);
+    console.log(user);
+    res.status(200).json({
+        "message" : "User registered",
+        "user" : {
+            "id" : id
+        }
+    })
+    id++;
 }) 
 // execute the func only when the req is on /register
 // in middlewares we dont define paths and let the function run for every incoming req.
 
+app.post('/login', (req,res) => {
+    const {email, password} = req.body;
+    
+    if(!email || !password){
+        return res.status(400).json({
+            message : "Email and password required to log in"
+        })
+    }
+    else if(users.some(user => user.email === email && user.password === password)){
+        return res.status(200).json({
+            message: `successfuly logged in.`   
+        })
+    }
+    
+    return res.status(400).json({
+        message : `user with email ${email} not found`
+    })
+})
 
-app.listen(4000, () => {
-    console.log("server running on 4000");
+
+app.listen(PORT, () => {
+    console.log("server running on", PORT);
 })
