259ccaa2d0107811630a405368fcb752501ff808
Adii-tii <sableaditi8@gmail.com>
2026-01-24 15:16:09 +0530

    refactored

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require('express'); //legacy way of importing
+const mongoose = require('mongoose');
+
 const app = express(); // configuring express app
 
 const PORT = 5001;
+mongoose.connect(process.env.MONGO_DB_CONNECTION_URL)
+    .then(() => console.log("connection established successfully!"))
+    .catch((error) => console.log("failed to establish connection"));
+
 app.use(express.json()); 
 
 const authRoutes = require('./src/routes/authRoutes');
