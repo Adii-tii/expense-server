735efc35d91e38026737055b3df7ec427958c13a
Adii-tii <sableaditi8@gmail.com>
2026-01-24 15:16:40 +0530

    added exception handling

--- src/dao/userDao.js
+++ src/dao/userDao.js
@@ -7,7 +7,21 @@ const userDao = {
 
     create: async(userData) => {
         const newUser = new User(userData);
-        return await newUser.save();
+        try{
+            return await newUser.save();
+        } catch (error){
+            if(error.code === 11000){
+                const err = new Error();
+                err.code = 'USER_EXISTS';
+                throw err;
+            }else{
+                console.log(error);
+                throw new Error({
+                    code: 'INTERNAL_SERVER_ERROR',
+                    message: 'Something went wrong while communicating with DB'
+                });
+            }
+        }
     }
 };
 
