238f57030c4bc9768e7bde325d6082a858943885
Adii-tii <sableaditi8@gmail.com>
2026-01-28 08:45:06 +0530

    added expense Dao

---
+++ src/dao/expenseDao.js
@@ -0,0 +1,10 @@
+const Expense = require("../models/expense");
+const { addMembers } = require("./groupDao");
+
+// addExpense
+// add or delete members from split
+// removeExpense
+// editExpense
+// getExpensesByStatus
+// getExpensesByUserStatus
+
